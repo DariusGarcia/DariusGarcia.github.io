f7472a4399c3a0c99c56a7e5fd2a45ad88ccf22d
Darius Garcia <dariusgarcia888@gmail.com>
2022-05-18 23:21:13 -0700

    Initial app setup

---
+++ src/components/About/about.jsx
@@ -0,0 +1,19 @@
+
+import './about.css';
+
+
+function About() {
+
+    return (
+        <div className="about-container">
+            <h2 id='hey'>Hey! I am </h2>
+            <h1 id="name">Darius Garcia.</h1>
+
+        </div>
+        
+        
+        
+    )
+}
+
+export default About;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+function Header() {
+
+    return (
+        <div className="header-container">
+            <ul className="nav-list">
+                <li>About Me</li>
+                <li>Projects</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
