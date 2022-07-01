fa004f90c5fe0339f2fd3b96ebf9c87998a7b139
Darius Garcia <dariusgarcia888@gmail.com>
2022-06-30 21:58:57 -0700

    fixed util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -36,3 +36,6 @@ export const fadeIn = {
     },
   },
 };
+
+export const openSpring = { type: "spring", stiffness: 200, damping: 30 };
+export const closeSpring = { type: "spring", stiffness: 300, damping: 35 };
